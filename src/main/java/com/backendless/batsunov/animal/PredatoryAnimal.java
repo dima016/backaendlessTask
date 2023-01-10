be9a097f3a8424885f42e8d9700c9a06d1979144
dbatsunov39441 <Unlimited_4_all!>
2023-01-10 11:09:47 +0200

    Updated predatory animal type to eat other animals

--- src/main/java/com/backendless/batsunov/animal/PredatoryAnimal.java
+++ src/main/java/com/backendless/batsunov/animal/PredatoryAnimal.java
@@ -3,7 +3,15 @@ package com.backendless.batsunov.animal;
 import com.backendless.batsunov.food.Meat;
 
 public abstract class PredatoryAnimal implements Animal {
-    public void eat(Meat food){
+    public void eat(Meat food) {
         System.out.println("Eating meat " + food);
+    }
+
+    public void eat(Animal animal) throws IllegalArgumentException {
+        if (animal == this) {
+            throw new IllegalArgumentException("Can't eat itself");
+        }
+
+        System.out.println("Eating animal " + animal);
     }
 }
